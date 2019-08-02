ebd70e09e1d866f07ad780b6e5d118bd024e1576
SantiagoASC6 <52749356+SantiagoASC6@users.noreply.github.com>
2019-08-02 14:31:23 -0400

    enjoy ;)

---
+++ main.js
@@ -0,0 +1,8 @@
+function pleaseWork(){
+    let hide = document.getElementById("img-thumbnail")
+        if(hide.style.visibility === "hidden"){
+            hide.style.visibility = "visible"
+        } else{
+            hide.style.visibility = "hidden";
+    }          
+}
